88b06879c24d9be845f92bc558c5391890bb0022
Chelsea <croland711@yandex.com>
2020-12-15 11:01:45 -0800

    commit tree 62c8a8fe938f3ad429f8bcf8dd3fd0c29ffcff6d

---
+++ db/migrations/001_create_costumes.rb
@@ -0,0 +1,13 @@
+class CreateCostumes < ActiveRecord::Migration[4.2]
+    def change
+      create_table :costumes do |t|
+        t.string :name
+        t.string :size
+        t.string :image_url
+        t.float :price
+  
+        t.timestamps null: false
+      end
+    end
+  end
+  
